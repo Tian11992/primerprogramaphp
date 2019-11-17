c7c8deff09865002ec82dd1f4c0c9ffd6f2ebc32
Sebastian Correa <sebas.11992@hotmail.com>
2019-11-17 12:53:07 -0500

    Se agrega hoja de estilo css

---
+++ biografia_kratos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Biografia de Kratos</title>
+    </head>
+    <body>
+        <h1>Biografia de Kratos</h1>
+        <p>A lo largo de la era griega de la serie, Kratos se embarca en varias aventuras en un intento por evitar un 
+            desastre o cambiar su destino. Generalmente se lo presenta como ajeno a todo lo demás y arrogante en la naturaleza.
+             A menudo se involucra en actividades moralmente ambiguas y realiza actos de extrema violencia. Se convirtió en el 
+             "Fantasma de Esparta" después de matar accidentalmente a su familia debido a los trucos de Ares. 
+             Más tarde, venga la muerte de su familia y se convierte en el dios de la guerra después de matar a Ares. 
+             Kratos finalmente se revela que es un semidiós y el hijo de Zeus, aunque luego Zeus lo traiciona.
+        </p>
+        <img id="imagen2" src="kratos2.jpg">
+        <br>
+        <a href="index.php">Volver</a>
+
+
+    </body>
+    
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>
+Hola Mundo
+</title>
+<link rel= "stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+<!-- Esto es un comentario-->
+<h1>Hola mundo, me llamo Sebastian</h1>
+<h2>Subtitulo</h2>
+<h3>Contenido de informacion</h3>
+<h4>Un texto pequeño</h4>
+<h5>Un texto mas pequeño</h5>
+<h6>Muy pequeño</h6>
+<p>Esto es un parrafo para colocar mucha informacion</p>
+<p>Tengo un texto con estilo<b>Negrita</b></p>
+<a href="http://www.google.com.co" target="_blank">Clic aquí para ir a google</a>
+<br>
+Hola mundo 2.
+<br>
+Este es otro texto.
+<br>
+<a href="http://localhost/primerprograma/biografia_kratos.php"><img src="kratos.jpg"></a>
+</body>
+</html>
